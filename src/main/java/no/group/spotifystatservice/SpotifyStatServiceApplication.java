61c847e1589012660585dda12594dcc7d83ab8c8
hyh4x <everhax@gmail.com>
2022-06-08 16:08:12 +0300

    base gradle project

---
+++ src/main/java/no/group/spotifystatservice/SpotifyStatServiceApplication.java
@@ -0,0 +1,13 @@
+package no.group.spotifystatservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyStatServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyStatServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/group/spotifystatservice/SpotifyStatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package no.group.spotifystatservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyStatServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
